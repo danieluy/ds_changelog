8fae910704f63d9960845d31f77ff54ff3c71601
danieluy <danielsosa.dev@gmail.com>
2017-02-14 17:57:11 -0300

    Lots of changes :)

--- classes/entry.js
+++ classes/entry.js
@@ -1,17 +1,31 @@
 "use strict";
 
 const Entry = function (values) {
+    this.id = values.id;
     this.version = values.version;
     this.type = values.type;
     this.date = values.date;
     this.message = values.message;
+    this.status = 1;
 }
 Entry.prototype.view = function (index) {
     let li = document.createElement('li');
     li.classList.add(`list-item`);
     li.classList.add(`${this.type.toLowerCase()}`);
     li.innerHTML = this.message;
-    li.setAttribute('data-i', index);
+    if(this.status === 0)
+        li.classList.add('deleted');
+
+    let actions = document.createElement('div');
+    actions.classList.add('actions-wrapper');
+
+    let close = document.createElement('button');
+    close.innerHTML = '&#10006';
+    close.classList.add('btn-close');
+    close.setAttribute('onclick', `app.deleteEntry(${this.id})`);
+    actions.appendChild(close);
+
+    li.appendChild(actions);
     return li;
 }
 
--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const File = require('./classes/file');
 // const files = require('./files.js');
 
-document.addEventListener('DOMContentLoaded', function () {
-  app.init();
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//   app.init();
+// });
 
 const app = {
   sort_criteria: undefined,
@@ -14,8 +14,10 @@ const app = {
   },
   DOMCache: function () {
     this.input_file = document.getElementById('input-file');
-    this.log_entries_list = document.getElementById('log-entries-list');
+    this.entries_list = document.getElementById('entries-list');
     this.btn_new_entry = document.getElementById('btn-new-entry');
+    this.btn_save = document.getElementById('btn-save');
+    this.todos_list = document.getElementById('todos-list');
     // Form add log entry
     this.form_add_log_entry = document.getElementById('form-add-log-entry');
     this.new_log_entry_type = document.getElementById('new-log-entry-type');
@@ -28,6 +30,7 @@ const app = {
   DOMListeners: function () {
     this.btn_new_entry.addEventListener('click', this.displaylogEntryForm.bind(this));
     this.input_file.addEventListener('change', this.openFile.bind(this));
+    this.btn_save.addEventListener('click', this.saveFile.bind(this));
   },
   displaylogEntryForm() {
     this.form_add_log_entry.parentNode.classList.remove('hidden');
@@ -37,40 +40,54 @@ const app = {
 
   },
   openFile: function (evt) {
-    if (!this.open_file) {
-      this.open_file = new File({
-        lastModified: evt.target.files[0].lastModified,
-        name: evt.target.files[0].name,
-        path: evt.target.files[0].path,
-        size: evt.target.files[0].size,
-        type: evt.target.files[0].type,
-        content: fs.readFileSync(evt.target.files[0].path, 'UTF-8')
-      })
-      console.log(this.open_file)
-      this.render.logEntries.call(this);
+    this.open_file = new File({
+      lastModified: evt.target.files[0].lastModified,
+      name: evt.target.files[0].name,
+      path: evt.target.files[0].path,
+      size: evt.target.files[0].size,
+      type: evt.target.files[0].type,
+      content: fs.readFileSync(evt.target.files[0].path, 'UTF-8')
+    })
+    console.log(this.open_file)
+    this.render.logEntries.call(this);
+  },
+  saveFile: function () {
+    if (this.open_file) {
+      const content = this.open_file.toSave();
+      console.log('' + this.open_file.origin.path)
+      fs.writeFileSync(this.open_file.origin.path + '_1', content, 'UTF-8')
     }
-    else {
-      alert('A file is already open')
-    }
+  },
+  deleteEntry: function (index) {
+    this.open_file.deleteEntry(index);
+    this.render.logEntries.call(this);
   },
   render: {
     logEntries: function () {
-      this.log_entries_list.innerHTML = '';
-      let ul = document.createElement('ul');
-      ul.setAttribute('class', 'entry-list');
-      ul.classList.add('entry-list');
-      let entries = [];
-      switch (this.sort_criteria) {
-        case 'type':
-          entries = this.open_file.entriesByType();
-          break;
-        default:
-          entries = this.open_file.entriesByDate();
-          break;
-      }
+      this.todos_list.innerHTML = '';
+      this.entries_list.innerHTML = '';
+      let ul_entries = document.createElement('ul');
+      ul_entries.classList.add('entry-list');
+      let ul_todos = document.createElement('ul');
+      ul_todos.classList.add('todos-list');
+      let entries = this.sortEntries();
       for (var i = 0; i < entries.length; i++)
-        ul.appendChild(entries[i].view(i));
-      this.log_entries_list.appendChild(ul);
+        if (entries[i].type !== 'T')
+          ul_entries.appendChild(entries[i].view(i));
+        else
+          ul_todos.appendChild(entries[i].view(i));
+      this.todos_list.appendChild(ul_todos);
+      this.entries_list.appendChild(ul_entries);
+    }
+  },
+  sortEntries: function () {
+    switch (this.sort_criteria) {
+      case 'type':
+        return this.open_file.entriesByType();
+      default:
+        return this.open_file.entriesByDate();
     }
   }
 }
+
+app.init();
